c54140ab0d98e44094c31988f3e2cac6f100c49a
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2015-12-15 15:11:47 +0200

    Added initial floorplan with bounding boxes.

---
+++ www/js/shipDirective.js
@@ -0,0 +1,69 @@
+
+// All the pictures are horizontally aligned with each other, at least within one image file.
+// The vertical alignment can be deduced from the common center line which can be calculated
+// from the bounding box without the texts respectively.
+const plan = {
+  legend: {
+    image: '/img/balticprincess_2_4.png',
+    bb: [310, 1046, 1223, 1149]
+  },
+  floors: [
+    {
+      floor: 2,
+      image: '/img/balticprincess_2_4.png',
+      bb: [114, 784, 1615, 989]
+    },
+    {
+      floor: 3,
+      image: '/img/balticprincess_2_4.png',
+      bb: [113, 495, 1589, 701]
+    },
+    {
+      floor: 4,
+      image: '/img/balticprincess_2_4.png',
+      bb: [113, 175, 1604, 381]
+    },
+    {
+      floor: 5,
+      image: '/img/balticprincess_5_8.png',
+      bb: [116, 1058, 1606, 1304]
+      bb_no_text: [116, 1072, 1606, 1276]
+    },
+    {
+      floor: 6,
+      image: '/img/balticprincess_5_8.png',
+      bb: [122, 784, 1492, 1005]
+      bb_no_text: [122, 784, 1491, 990]
+    },
+    {
+      floor: 7,
+      image: '/img/balticprincess_5_8.png',
+      bb: [130, 489, 1472, 708]
+    },
+    {
+      floor: 8,
+      image: '/img/balticprincess_5_8.png',
+      bb: [142, 208, 1449, 143]
+    },
+    {
+      floor: 9,
+      image: '/img/balticprincess_9_12.png',
+      bb: [142, 1180, 1432, 1385]
+    },
+    {
+      floor: 10,
+      image: '/img/balticprincess_9_12.png',
+      bb: [208, 876, 1426, 1113]
+    },
+    {
+      floor: 11,
+      image: '/img/balticprincess_9_12.png',
+      bb: [244, 588, 1426, 825]
+    },
+    {
+      floor: 12,
+      image: '/img/balticprincess_9_12.png',
+      bb: [332, 348, 1386, 488]
+    },
+  ]
+};
