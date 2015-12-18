5df1c2dd208aad9fa3bc63ef841d65dd14619339
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2015-12-18 16:13:22 +0200

    Removed useless server-side Facebook auth. Added APIs for setting user info and position to Couchdb.

--- server.js
+++ server.js
@@ -4,17 +4,10 @@ const app = express();
 const nano = require('nano')('http://localhost:5984');
 const passport = require('passport');
 
-const FacebookStrategy = require('passport-facebook').Strategy;
 const FACEBOOK_APP_ID = '1678939655686347';
 const keys = require('./keys.json');
 // Remember to set this to correct value. Don't put it in GitHub.
 const FACEBOOK_APP_SECRET = keys.FACEBOOK_APP_SECRET;
-// For local testing, set sviitti in hosts file to point to 127.0.0.1
-// This is needed for Facebook redirect authentication.
-// For mobile clients we can't use this DNS name, we should have a stable server on the net.
-// This address must be specifically allowed in the Facebook app settings:
-// https://developers.facebook.com/apps/1678939655686347/settings/
-const SERVER_ADDRESS = 'http://sviitti.td.local:8080';
 
 const sviitti_db = nano.db.use('sviitti');
 
@@ -41,29 +34,18 @@ app.use(function(req, res, next) {
   next();
 });
 
-passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: SERVER_ADDRESS + '/auth/facebook/callback',
-    enableProof: false
-  },
-  function(accessToken, refreshToken, profile, done) {
-    return done(null, profile);
-  }
-));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/auth/facebook',
-  passport.authenticate('facebook'));
-
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
+app.put('/auth/:mac',
   function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
+    // FIXME: Here we should put the client user info (name, fbid, btid, photo) to CouchDB indexed by MAC address.
 });
+
+app.put('/pos/:mac',
+    function(req, res) {
+      // FIXME: Here we should put the latest client position info (bsid) to CouchDB indexed by MAC address.
+  });
 
 var server = app.listen(PORT, function () {
   var host = server.address().address;
