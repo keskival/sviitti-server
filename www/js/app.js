3ad0da3ea527ff8fc6cd1f09680231d33d121c84
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2016-01-03 20:52:27 +0200

    Removed the default mock content.

--- www/js/app.js
+++ www/js/app.js
@@ -145,37 +145,6 @@ angular.module('sviitti', ['ionic', 'ngCordova', 'LocalStorageModule',
       }
   })
 
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/welcome');
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,5 @@
 angular.module('sviitti.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
 .controller('WelcomeCtrl', function($scope, $rootScope, $cordovaOauth, $http, $timeout) {
   $scope.setNickname = function (nickname) {
     $timeout(function() {
